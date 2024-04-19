dbcf825967623fa823b2e47d3caf4841c0222122
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2024-04-19 04:54:22 +0200

    Finishging

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import { useState } from "react";
-
 const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
-
 function App() {
   let [step, setStep] = useState(0);
+  const [isOpen, setIsIpen] = useState(true);
   function handlePervious() {
-    if (step) setStep(step - 1);
+    if (step) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 2) setStep(step + 1);
+    if (step < 2) setStep((s) => s + 1);
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 0 ? "active" : ""}>1</div>
-        <div className={step >= 1 ? "active" : ""}>2</div>
-        <div className={step >= 2 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step + 1}: {messages[step]}
-        {test.name}
-      </p>
-      <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#ffff" }} onClick={handlePervious}>
-          Pervious
-        </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#ffff" }} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsIpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 0 ? "active" : ""}>1</div>
+            <div className={step >= 1 ? "active" : ""}>2</div>
+            <div className={step >= 2 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            {step >= 0 && step <= 2 && (
+              <>
+                Step {step + 1}: {messages[step]}
+              </>
+            )}
+          </p>
+          <div className="buttons">
+            <button style={{ backgroundColor: "#7950f2", color: "#ffff" }} onClick={handlePervious}>
+              Pervious
+            </button>
+            <button style={{ backgroundColor: "#7950f2", color: "#ffff" }} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 export default App;
